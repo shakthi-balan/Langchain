93b433884dfee3142ed37b6559b81c29240cb4c7
Shakthi Balan <s.shakthibalan@gmail.com>
2025-08-15 11:06:00 +0530

    linkedin-search-agent

---
+++ main.py
@@ -0,0 +1,38 @@
+from dotenv import load_dotenv
+from langchain.prompts.prompt import PromptTemplate
+from langchain_openai import ChatOpenAI
+
+from third_parties.linkedin import scrape_linkedin_profile
+from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
+
+
+def ice_break_with(name: str) -> str:
+    ### uncomment for real use case
+    linkedin_username = linkedin_lookup_agent(name=name)
+    linkedin_username = "https://www.linkedin.com/in/eden-marco/   "
+    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_username, mock=True)
+
+    summary_template = """
+    given the Linkedin information {information} about a person I want you to create:
+    1. A short summary
+    2. two interesting facts about them
+    """
+    summary_prompt_template = PromptTemplate(
+        input_variable  =   ["information"], 
+        template        =   summary_template
+    )
+
+    llm = ChatOpenAI(temperature=0, model_name="gpt-4o-mini ")
+
+    chain = summary_prompt_template | llm
+
+    res = chain.invoke(input={"information": linkedin_data})
+
+    print(res)
+
+
+if __name__ == "__main__":
+    load_dotenv()
+
+    print("Ice Breaker Enter")
+    ice_break_with(name="Eden Marco")
